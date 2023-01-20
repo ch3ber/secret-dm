a01dca6e3eafbae321e1184d671c78244dd4a657
Eber Alejo <eber4202@hotmail.com>
2023-01-20 11:45:07 -0600

    CHORE: config env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
-import './globals.css'
+import '../styles/globals.css'
+import { ReactNode } from 'react'
 
-export default function RootLayout({
-  children,
+export default function RootLayout ({
+  children
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
   return (
     <html lang="en">
@@ -12,7 +13,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className='bg-black text-white'>{children}</body>
     </html>
   )
 }
